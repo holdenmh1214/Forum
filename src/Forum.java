cd86c0f0a5f59b8223609ba00df694c35daee1b8
holdenhughes <holdenmh121489@gmail.com>
2015-10-15 12:06:29 -0400

    Read posts and display replies

--- src/Forum.java
+++ src/Forum.java
@@ -1,12 +1,44 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by holdenhughes on 10/15/15.
  */
 public class Forum {
     public static void main(String[] args) {
+        ArrayList<Post> posts = new ArrayList();
+        String postContent = readFile("posts.txt");
+        String[] lines = postContent.split("\n");
+
+        for (String line : lines) {
+            String[] columns = line.split("\\|");
+            int replyId = Integer.valueOf(columns[0]);
+            String author = columns[1];
+            String text = columns[2];
+            Post post = new Post(replyId, author,text);
+            posts.add(post);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int postId = -1;
+        while (true) {
+            printPost(posts, postId);
+            System.out.println("Enter post id to see replies");
+            postId = Integer.valueOf(scanner.nextLine());
+        }
+    }
+
+    static void printPost(ArrayList<Post> posts, int postId) {
+        int id =0;
+        for (Post post : posts) {
+            if (post.replyID == postId) {
+                System.out.println(String.format("[%d] %s (%s)", id, post.text, post.author));
+            }
+            id++;
+        }
 
     }
 
---
+++ src/Post.java
@@ -0,0 +1,15 @@
+/**
+ * Created by holdenhughes on 10/15/15.
+ */
+public class Post {
+    int replyID;
+    String author;
+    String text;
+
+    public Post(int replyID, String author, String text) {
+        this.replyID = replyID;
+        this.author = author;
+        this.text = text;
+    }
+
+}
